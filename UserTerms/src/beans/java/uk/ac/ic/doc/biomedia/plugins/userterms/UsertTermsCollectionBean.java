e103dde47ed4fdcb3627e3e9d633f21f46ad8612
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-11-08 18:26:07 +0000

    [TERMS] added deployToPluginFolder gradle task

--- UserTerms/src/beans/java/uk/ac/ic/doc/biomedia/plugins/userterms/UsertTermsCollectionBean.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ic.doc.biomedia.plugins.userterms;
-
-import org.nrg.xdat.bean.XnatProjectdataBean;
-
-/**
- * Created by gmlenz on 07/11/16.
- */
-public class UsertTermsCollectionBean extends XnatProjectdataBean {
-}
