fc26b37605b6640dcf48aee3d5d2814598f905f0
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-11-08 17:21:37 +0000

    LDAP plugin

---
+++ LdapBiomedia/src/main/java/uk/ac/imperial/xnat/workshop/WorkshopXnatPlugin.java
@@ -0,0 +1,11 @@
+package uk.ac.imperial.xnat.workshop;
+
+import org.nrg.framework.annotations.XnatPlugin;
+/**
+ * Created by gmlenz on 31/08/16.
+ */
+@XnatPlugin(value = "ldap1-xnat-plugin", name = "LDAP custom config", description = "Temporary solution to enable LDAP")
+public class WorkshopXnatPlugin {
+
+
+}
