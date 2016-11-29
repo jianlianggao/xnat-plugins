15d65de1387ee3d7975ca8f582ad0826cf9061c5
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-11-29 18:04:19 +0000

    [LDAP] added repository file

---
+++ LdapBiomedia/src/main/java/uk/ac/ic/doc/biomedia/dhcpldap/repositories/DhcpLdapRepository.java
@@ -0,0 +1,14 @@
+package uk.ac.ic.doc.biomedia.dhcpldap.repositories;
+
+import org.nrg.framework.orm.hibernate.AbstractHibernateDAO;
+import org.springframework.stereotype.Repository;
+import uk.ac.ic.doc.biomedia.dhcpldap.entities.DhcpLdap;
+
+/**
+ * Created by gmlenz on 29/11/16.
+ */
+
+@Repository
+public class DhcpLdapRepository extends AbstractHibernateDAO<DhcpLdap> {
+
+}
