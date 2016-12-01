5657a3df2c261e3e00c19ca5610608b3e5f83a46
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-01 16:39:43 +0000

    [LDAP] added openContext() to connect to server

--- LdapBiomedia/src/main/java/uk/ac/ic/doc/biomedia/dhcpldap/services/DhcpLdapServiceInterface.java
+++ LdapBiomedia/src/main/java/uk/ac/ic/doc/biomedia/dhcpldap/services/DhcpLdapServiceInterface.java
@@ -4,10 +4,16 @@ import org.apache.xpath.operations.Bool;
 import org.nrg.framework.orm.hibernate.BaseHibernateService;
 import uk.ac.ic.doc.biomedia.dhcpldap.entities.DhcpLdap;
 
+import javax.naming.NamingException;
+
 /**
  * Created by gmlenz on 29/11/16.
  */
 public interface DhcpLdapServiceInterface extends BaseHibernateService<DhcpLdap> {
 
     Boolean createNewDhcpLdapUser(final String username);
+    Boolean updateDhcpLdapUser(final String username);
+    void openContext(final String adminUser, final String adminPassword) throws NamingException;
+
+
 }
--- LdapBiomedia/src/main/java/uk/ac/ic/doc/biomedia/dhcpldap/services/impl/DhcpLdapService.java
+++ LdapBiomedia/src/main/java/uk/ac/ic/doc/biomedia/dhcpldap/services/impl/DhcpLdapService.java
@@ -7,6 +7,11 @@ import uk.ac.ic.doc.biomedia.dhcpldap.entities.DhcpLdap;
 import uk.ac.ic.doc.biomedia.dhcpldap.repositories.DhcpLdapRepository;
 import uk.ac.ic.doc.biomedia.dhcpldap.services.DhcpLdapServiceInterface;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.ldap.InitialLdapContext;
+import java.util.Hashtable;
+
 /**
  * Created by gmlenz on 29/11/16.
  */
@@ -18,5 +23,29 @@ public class DhcpLdapService extends AbstractHibernateEntityService<DhcpLdap, Dh
     @Override
     public Boolean createNewDhcpLdapUser(String username) {
         return null;
+    }
+
+    @Override
+    public Boolean updateDhcpLdapUser(String username) {
+        return null;
+    }
+
+    @Override
+    public void openContext(String adminUser, String adminPassword) throws NamingException {
+        DhcpLdap ldap = new DhcpLdap();
+        Hashtable<String, String> envVars = ldap.getEnvironmentVariables();
+        envVars.put(Context.SECURITY_PRINCIPAL, adminUser);
+        envVars.put(Context.SECURITY_CREDENTIALS, adminPassword);
+        envVars.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        envVars.put(Context.SECURITY_AUTHENTICATION, "simple");
+        envVars.put(Context.PROVIDER_URL, ldap.getLDAP_HOST());
+        envVars.put(Context.SECURITY_PROTOCOL, "ssl");
+        envVars.put(Context.REFERRAL, "follow");
+
+        try {
+            ldap.setLdapContext(new InitialLdapContext(envVars, null));
+
+        } catch (NamingException e) { throw e; }
+
     }
 }
