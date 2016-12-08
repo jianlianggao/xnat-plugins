f7955e834b7bc1913f1966ae4addb209b08e5beb
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-08 22:59:10 +0000

    [TERMS] accomodate for change in function names

--- UserTerms/src/main/java/org/nrg/xnat/turbine/modules/actions/CheckTerms.java
+++ UserTerms/src/main/java/org/nrg/xnat/turbine/modules/actions/CheckTerms.java
@@ -3,11 +3,15 @@ package org.nrg.xnat.turbine.modules.actions;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.XDAT;
+import org.nrg.xdat.entities.XdatUserAuth;
+import org.nrg.xdat.services.XdatUserAuthService;
 import org.nrg.xdat.turbine.modules.actions.SecureAction;
 import org.nrg.xdat.turbine.utils.TurbineUtils;
 import org.nrg.xft.security.UserI;
 import org.nrg.xnat.biomedia.userAttributes.entities.UserAttributes;
 import org.nrg.xnat.biomedia.userAttributes.services.UserAttributesService;
+
+import java.util.List;
 
 /**
  * Created by gmlenz on 08/12/16.
@@ -21,10 +25,20 @@ public class CheckTerms extends SecureAction {
 
         UserAttributesService service = XDAT.getContextService().getBean(UserAttributesService.class);
         String username = user.getUsername();
-        try {
-            UserAttributes extendedUser = service.findByUsername(username);
 
-            if (extendedUser.hasAcceptedTerms()) {
+        //Check if there is already an UserAttributes instance for this user
+ /*       try {
+            UserAttributes thisUser = service.findByUsername(username);
+        } catch (Exception e) {
+            service.create(new UserAttributes(username));
+        }*/
+
+        service.create(new UserAttributes(username, false));
+
+        try {
+            UserAttributes extendedUser = service.findByUserName(username);
+
+            if (extendedUser.isAcceptedTerms()) {
                 data.setMessage("Already signed the User Terms!");
                 data.setScreenTemplate("Index.vm");
             } else {
