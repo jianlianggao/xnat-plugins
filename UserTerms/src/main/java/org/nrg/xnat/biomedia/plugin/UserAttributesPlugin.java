e889b9badebbe4e7309c76519a3825cd7bf4a250
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-08 17:54:29 +0000

    [TERMS] removed redundant userattributes data model

--- UserTerms/src/main/java/org/nrg/xnat/biomedia/plugin/UserAttributesPlugin.java
+++ UserTerms/src/main/java/org/nrg/xnat/biomedia/plugin/UserAttributesPlugin.java
@@ -4,16 +4,14 @@ import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.nrg.xdat.bean.BiomediaUserattributescollectionBean;
+//import org.nrg.xdat.bean.BiomediaUserattributescollectionBean;
 
 /**
  * Created by gmlenz on 31/08/16.
  */
 @XnatPlugin(value = "userattributes-plugin", name = "Custom user attributes",
         description = "Store information about user's attributes",
-        entityPackages = "org.nrg.xnat.biomedia.userAttributes.entities",
-        dataModels = {@XnatDataModel(value = BiomediaUserattributescollectionBean.SCHEMA_ELEMENT_NAME,
-                singular = "User Attribute", plural = "User Attributes")})
+        entityPackages = "org.nrg.xnat.biomedia.userAttributes.entities")
 @ComponentScan({"org.nrg.xnat.biomedia.userAttributes.services.impl",
         "org.nrg.xnat.biomedia.userAttributes.repositories",
         "org.nrg.xnat.biomedia.userAttributes.rest",
