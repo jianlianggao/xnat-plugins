426b80d0ca9bba729233e17847c55ca6740a7e87
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-08 18:00:43 +0000

    [TERMS] service interface clean up

--- UserTerms/src/main/java/org/nrg/xnat/biomedia/userAttributes/services/UserAttributesService.java
+++ UserTerms/src/main/java/org/nrg/xnat/biomedia/userAttributes/services/UserAttributesService.java
@@ -7,19 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface UserAttributesService extends BaseHibernateService<UserAttributes> {
-    /**
-     * Finds the subject with the indicated {@link UserAttributes#getSubjectId() subject ID}.
-     *
-     * @param subjectId The subject ID.
-     *
-     * @return The subject with the indicated ID, null if not found.
-     */
-
-    UserAttributes attachUserAttributes(String user, Boolean terms);
 
     UserAttributes findByUsername(final String username);
-
-    UserAttributes findBySubjectId(final String subjectId);
 
     String getStringAttributeForUser(final String user, String key);
 
