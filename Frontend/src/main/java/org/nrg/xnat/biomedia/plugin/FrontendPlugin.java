c03899e77a5ae656c542ef3772a18c4bae1a5275
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-12 14:22:14 +0000

    [Frontend] moved modifications of the frontend in dhcp-xnat into separate plugin. app.css not being picked up

---
+++ Frontend/src/main/java/org/nrg/xnat/biomedia/plugin/FrontendPlugin.java
@@ -0,0 +1,20 @@
+package org.nrg.xnat.biomedia.plugin;
+
+import org.nrg.framework.annotations.XnatDataModel;
+import org.nrg.framework.annotations.XnatPlugin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+//import org.nrg.xdat.bean.BiomediaUserattributescollectionBean;
+
+/**
+ * Created by gmlenz on 31/08/16.
+ */
+@XnatPlugin(value = "frontend-plugin", name = "Custom XNAT frontend",
+        description = "Frontend modifications for dHCP")
+public class FrontendPlugin {
+    @Bean
+    public String testMessage() {
+        return "Hello there from the UserAttributes plugin!";
+    }
+
+}
