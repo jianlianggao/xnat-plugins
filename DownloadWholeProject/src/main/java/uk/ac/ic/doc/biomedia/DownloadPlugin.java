2fc1c9f4e0f57019a89621f980f3296f767fdaac
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-13 16:41:16 +0000

    [Download] Added option in .vm for direct download

---
+++ DownloadWholeProject/src/main/java/uk/ac/ic/doc/biomedia/DownloadPlugin.java
@@ -0,0 +1,25 @@
+/*
+ * xnat-workshop-plugin: WorkshopXnatPlugin
+ * XNAT http://www.xnat.org
+ * Copyright (c) 2016, Washington University School of Medicine
+ * All Rights Reserved
+ *
+ * Released under the Simplified BSD.
+ */
+
+package uk.ac.ic.doc.biomedia;
+
+import org.nrg.framework.annotations.XnatDataModel;
+import org.nrg.framework.annotations.XnatPlugin;
+//import org.nrg.xdat.bean.WorkshopBiosamplecollectionBean;
+//import org.nrg.xdat.bean.RadRadiologyreaddataBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@XnatPlugin(value = "downloadPlugin", name = "Adds download option to pull tarball of data via http")
+public class DownloadPlugin {
+    @Bean
+    public String workshopPluginMessage() {
+        return "Hello there from the download plugin!";
+    }
+}
