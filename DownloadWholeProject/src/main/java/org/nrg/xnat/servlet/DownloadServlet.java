0166029357a0166de18ff4edacf9ce9f2c173ea2
Gregor Lenz <g.lenz@imperial.ac.uk>
2016-12-14 22:45:20 +0000

    [Download] added servlet that makes external folder available

---
+++ DownloadWholeProject/src/main/java/org/nrg/xnat/servlet/DownloadServlet.java
@@ -0,0 +1,26 @@
+package org.nrg.xnat.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * Created by gmlenz on 14/12/16.
+ */
+@WebServlet("/tarballs2/*")
+public class DownloadServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        String filename = request.getPathInfo().substring(1);
+        File file = new File("/data/xnat/home/tarballs", filename);
+        response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        response.setHeader("Content-Disposition", "inline; filename=\"" + filename + "\"");
+        Files.copy(file.toPath(), response.getOutputStream());
+    }
+}
