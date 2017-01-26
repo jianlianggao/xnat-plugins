1b34790bb3619c01f62a6a294369f5882b625a80
Gregor Lenz <g.lenz@imperial.ac.uk>
2017-01-26 18:26:25 +0000

    [Download] Turbine action for downloading .torrent file but currently unused since it's not called from a form

--- DownloadWholeProject/src/main/java/org/nrg/xnat/turbine/modules/actions/TorrentDownloadAction.java
+++ DownloadWholeProject/src/main/java/org/nrg/xnat/turbine/modules/actions/TorrentDownloadAction.java
@@ -3,6 +3,7 @@ package org.nrg.xnat.turbine.modules.actions;
 import org.apache.turbine.util.RunData;
 import org.apache.velocity.context.Context;
 import org.nrg.xdat.turbine.modules.actions.SecureAction;
+import org.nrg.xdat.turbine.utils.TurbineUtils;
 
 /**
  * Created by gmlenz on 10/01/17.
@@ -10,6 +11,11 @@ import org.nrg.xdat.turbine.modules.actions.SecureAction;
 public class TorrentDownloadAction extends SecureAction {
     @Override
     public void doPerform(RunData data, Context context) throws Exception {
+
+        String server = TurbineUtils.GetFullServerPath();
+        String tarballPath = server + "tarballs/dhcp_rel_1.tar.gz";
+        data.setRedirectURI(tarballPath);
+
         data.setMessage("Download started");
         data.setScreenTemplate("Index.vm");
     }
